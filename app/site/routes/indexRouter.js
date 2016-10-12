d2da2a0011300662c38e01162501b40e374102d8
SergeyKotyushkin <sergey.kotyushkin@firstlinesoftware.com>
2016-10-12 16:10:16 +0300

    Redirect authenticated user from index

--- app/site/routes/indexRouter.js
+++ app/site/routes/indexRouter.js
@@ -4,26 +4,33 @@ function init(router) {
 	var userModel = require(__common + "/models/user");
 
 	router.get('/:culture', function (req, res, next) {
-		if (constants.labels.hasCulture(req.params.culture))
+		if (constants.labels.hasCulture(req.params.culture)) {
+			if(req.user.isAuthenticated) {
+				res.redirect(['/', req.params.culture, '/center'].join(''));
+				return;
+			}
+
 			res.render('index', settings.extended(req, { user: req.user }));
-		else
-			res.redirect('/' + constants.settings.common.defaultCulture);
+		} else {
+			res.redirect(['/', constants.settings.common.defaultCulture].join(''));
+		}
 	});
 
 	router.get('/', function (req, res, next) {
 		if (req.cookies.culture)
-			res.redirect('/' + req.cookies.culture);
+			res.redirect(['/', req.cookies.culture].join(''));
 		else
-			res.redirect('/' + constants.settings.common.defaultCulture);
+			res.redirect(['/', constants.settings.common.defaultCulture].join(''));
 	});
 
 	router.get('/:culture/center', function (req, res, next) {
 		if(!req.user.isAuthenticated) {
-			res.redirect('/' + req.params.culture);
+			res.redirect(['/', req.params.culture].join(''));
 			return;
 		}
 
 		res.write("center");
+		res.write(" email: " + req.user.email);
 		res.end();
 	});
 }
